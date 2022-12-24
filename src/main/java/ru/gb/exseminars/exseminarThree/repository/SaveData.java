c4d9bd2784430cd0aeec4e1d7719f22758ceaf97
Timofei Shadrin <tshadrin@yandex.ru>
2022-12-24 14:51:22 +0300

    output fixed

--- src/main/java/ru/gb/exseminars/exseminarThree/repository/SaveData.java
+++ src/main/java/ru/gb/exseminars/exseminarThree/repository/SaveData.java
@@ -2,6 +2,7 @@ package ru.gb.exseminars.exseminarThree.repository;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class SaveData {
     private final String data;
@@ -12,9 +13,12 @@ public class SaveData {
         this.fileName = fileName;
     }
 
-    public void writeFile() throws IOException{
-        try (FileWriter fileWriter = new FileWriter( fileName + ".txt", true)) {
-            fileWriter.append(data);
+    public void writeFile() throws IOException {
+        try (FileWriter fileWriter = new FileWriter(fileName + ".txt", true)) {
+            String result = Arrays.stream(data.trim().split(" "))
+                    .map(s -> "<" + s + ">")
+                    .reduce("", (s, s2) -> s + s2);
+            fileWriter.append(result);
             fileWriter.append('\n');
             fileWriter.flush();
         } catch (IOException e) {
